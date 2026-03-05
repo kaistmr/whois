8220da1c124cbed9800b070e11387f60b2bad41f
gyeongseok8727 <gyeongseok8727@gmail.com>
2026-03-05 18:55:30 +0900

    debug

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API_BASE = "https://script.google.com/macros/s/AKfycbwwjUXi8W5m4qrGkIuPFCPnkAgSmCfSLSSqMuLlqia4oxrbSW3tHJA-0FmhxOXUfrBI/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbxYMHdwcvIsGupIIR4MEod1hXmweP0ozm_y5FbxZsYq-y-MnH2uURZT2YRGGysISbcF/exec";
 const SURVEY_URL = "https://forms.gle/BkaXfCUPz3hYhc9F7";
